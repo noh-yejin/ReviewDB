b83507b0901952bd8a14bcea2811b822ebbe092f
yejin <shdpwls2838@naver.com>
2025-06-22 19:33:49 +0900

    add gitignore unnecessary files

--- backend/db/queries.py
+++
@@ -1,9 +0,0 @@
-def get_review_summary(cursor):
-    cursor.execute("SELECT * FROM Reviews LIMIT 5")
-    head = cursor.fetchall()
-
-    cursor.execute("SELECT COUNT(*) AS total FROM Reviews")
-    total = cursor.fetchone()["total"]
-
-    return head, total
-
